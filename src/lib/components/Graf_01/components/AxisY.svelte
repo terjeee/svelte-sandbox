ce00d6b5daee5b75a148845cca9de382080d45b5
terjeee <96795863+terjeee@users.noreply.github.com>
2023-05-27 01:26:46 +0200

    xd

---
+++ src/lib/components/Graf_01/components/AxisY.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let yTicks: number[];
+	export let yScale: d3.ScaleLinear<number, number, never>;
+</script>
+
+<g>
+	{#each yTicks as tick}
+		<text x="0" y={yScale(tick)}>{tick}</text>
+	{/each}
+</g>
